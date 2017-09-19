200408a9147ee3d2d57db5da74ad2340287632d4
Lin Xianyi <iynaix@gmail.com>
2017-09-19 15:28:19 +0800

    Removed dependency on combinations library.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,62 @@ import filterSums from "./kakuro"
 // bulmaswatch superhero theme for styling
 import "./bulmaswatch.min.css"
 
-const Input = ({ type = "text", ...inputAttrs }) =>
+const cleanNumberArray = numStr => {
+    // strip anything that isnt a number
+    numStr = numStr.replace(/[^1-9]/g, "")
+    return numStr.split("").map(n => parseInt(n, 10))
+}
+
+const Input = ({ name, type = "text", ...props }) =>
     <div className="control">
-        <input className="input" type={type} {...inputAttrs} />
+        <input name={name} className="input" type={type} {...props} />
     </div>
 
+const Result = ({ children, includes = "" }) => {
+    // strip anything that isnt a number
+    includes = includes.replace(/[^1-9]/g, "")
+
+    // replace includes with greyed out versions
+    return (
+        <p className="is-size-3">
+            {children.map(c =>
+                <span style={includes.includes(c) ? { opacity: 0.2 } : {}}>{c}</span>
+            )}
+        </p>
+    )
+}
+
 class App extends Component {
+    state = { length: "", total: "", includes: "", excludes: "" }
+
+    onHandleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onReset = e => {
+        this.setState({ length: "", total: "", includes: "", excludes: "" })
+    }
+
     render() {
-        const result = filterSums({
-            length: 3,
-            total: 20,
-            excludes: [3, 4],
-        })
+        const { length, total, includes, excludes } = this.state
+        // length and total are mandatory
+        const result = length && total
+            ? filterSums(parseInt(total, 10), parseInt(length, 10), {
+                  includes: cleanNumberArray(includes),
+                  excludes: cleanNumberArray(excludes),
+              })
+            : []
 
         return (
             <div>
                 <section className="hero is-dark is-bold has-text-centered">
                     <div className="hero-body">
                         <div className="container">
-
-                            <h1 className="title is-uppercase">Kakuro Helper</h1>
+                            <h1 className="title is-uppercase">
+                                <a onClick={this.onReset}>
+                                    Kakuro Helper
+                                </a>
+                            </h1>
                         </div>
                     </div>
                 </section>
@@ -31,38 +67,50 @@ class App extends Component {
                 <section className="section columns">
                     <div className="column">
                         <div className="has-text-centered is-size-4 has-text-weight-bold">
-                            Length
+                            Total
                         </div>
-                        <Input type="number" min={1} max={9} />
+                        <Input
+                            name="total"
+                            type="number"
+                            value={total}
+                            min={1}
+                            max={45}
+                            onChange={this.onHandleChange}
+                        />
                     </div>
                     <div className="column">
                         <div className="has-text-centered is-size-4 has-text-weight-bold">
-                            Total
+                            Length
                         </div>
-                        <Input type="number" min={1} max={45} />
+                        <Input
+                            name="length"
+                            type="number"
+                            value={length}
+                            min={1}
+                            max={9}
+                            onChange={this.onHandleChange}
+                        />
                     </div>
                     <div className="column">
                         <div className="has-text-centered is-size-4 has-text-weight-bold">
                             Includes
                         </div>
-                        <Input />
+                        <Input name="includes" value={includes} onChange={this.onHandleChange} />
                     </div>
                     <div className="column">
                         <div className="has-text-centered is-size-4 has-text-weight-bold">
                             Excludes
                         </div>
-                        <Input />
+                        <Input name="excludes" value={excludes} onChange={this.onHandleChange} />
                     </div>
                 </section>
 
                 <section className="section columns">
                     <div className="column" />
-                    <div className="column">
-                        {result.map((a, i) =>
-                            <p key={i} className="is-size-3">
-                                {a.join("")}
-                            </p>
-                        )}
+                    <div className="column has-text-centered">
+                        {result.length
+                            ? result.map((a, i) => <Result key={i} includes={includes}>{a}</Result>)
+                            : <p className="is-size-3">No results found.</p>}
                     </div>
                     <div className="column" />
                 </section>
--- src/kakuro.js
+++ src/kakuro.js
@@ -1,38 +1,76 @@
-import { range, sum, isArray, intersection } from "lodash/fp"
-import combinations from "combinations"
+import { sum, intersection } from "lodash/fp"
 
-const kakuroSums = () => combinations(range(1, 10))
+// implementation from: https://gist.github.com/axelpale/3118596
+const combinations = (set, k) => {
+    var i, j, combs, head, tailcombs
+
+    // There is no way to take e.g. sets of 5 elements from
+    // a set of 4.
+    if (k > set.length || k <= 0) {
+        return []
+    }
+
+    // K-sized set has only one K-sized subset.
+    if (k === set.length) {
+        return [set]
+    }
+
+    // There is N 1-sized subsets in a N-sized set.
+    if (k === 1) {
+        combs = []
+        for (i = 0; i < set.length; i++) {
+            combs.push([set[i]])
+        }
+        return combs
+    }
+
+    // Assert {1 < k < set.length}
+
+    // Algorithm description:
+    // To get k-combinations of a set, we want to join each element
+    // with all (k-1)-combinations of the other elements. The set of
+    // these k-sized sets would be the desired result. However, as we
+    // represent sets with lists, we need to take duplicates into
+    // account. To avoid producing duplicates and also unnecessary
+    // computing, we use the following approach: each element i
+    // divides the list into three: the preceding elements, the
+    // current element i, and the subsequent elements. For the first
+    // element, the list of preceding elements is empty. For element i,
+    // we compute the (k-1)-computations of the subsequent elements,
+    // join each with the element i, and store the joined to the set of
+    // computed k-combinations. We do not need to take the preceding
+    // elements into account, because they have already been the i:th
+    // element so they are already computed and stored. When the length
+    // of the subsequent list drops below (k-1), we cannot find any
+    // (k-1)-combs, hence the upper limit for the iteration:
+    combs = []
+    for (i = 0; i < set.length - k + 1; i++) {
+        // head is a list that includes only our current element.
+        head = set.slice(i, i + 1)
+        // We take smaller combinations from the subsequent elements
+        tailcombs = combinations(set.slice(i + 1), k - 1)
+        // For each (k-1)-combination we join it with the current
+        // and store it to the set of k-combinations.
+        for (j = 0; j < tailcombs.length; j++) {
+            combs.push(head.concat(tailcombs[j]))
+        }
+    }
+    return combs
+}
 
 export const printSums = sums => sums.map(s => s.join(""))
 
-export default ({ length, total, includes = [], excludes = [] } = {}) => {
-    let ret = kakuroSums()
-
-    // filter by length
-    if (length) {
-        ret = ret.filter(s => s.length === length)
-    }
-
-    // filter by sum
-    if (total) {
-        ret = ret.filter(s => sum(s) === total)
-    }
+export default (total, length, { includes = [], excludes = [] } = {}) => {
+    // filter by length and total
+    let ret = combinations([1, 2, 3, 4, 5, 6, 7, 8, 9], length).filter(s => sum(s) === total)
 
     // filter by includes
-    if (includes) {
-        if (!isArray(includes)) {
-            includes = [includes]
-        }
-
+    if (includes.length) {
         ret = ret.filter(s => intersection(s, includes).length === includes.length)
     }
 
     // filter by excludes
-    if (excludes) {
-        if (!isArray(excludes)) {
-            excludes = [excludes]
-        }
-
+    if (excludes.length) {
         ret = ret.filter(s => intersection(s, excludes).length === 0)
     }
 
