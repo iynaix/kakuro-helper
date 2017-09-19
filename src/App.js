ea30fd4f8ae552aac200f46a054a5998a9c3abe0
Lin Xianyi <iynaix@gmail.com>
2017-09-19 12:35:19 +0800

    Added bulma for css.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from "react"
-import filterSums, { printSums } from "./kakuro"
+import filterSums from "./kakuro"
 
-import "./App.css"
+// bulmaswatch superhero theme for styling
+import "./bulmaswatch.min.css"
+
+const Input = ({ type = "text", ...inputAttrs }) =>
+    <div className="control">
+        <input className="input" type={type} {...inputAttrs} />
+    </div>
 
 class App extends Component {
     render() {
-        console.log(
-            printSums(
-                filterSums({
-                    length: 3,
-                    total: 20,
-                    excludes: [3, 4],
-                })
-            )
-        )
+        const result = filterSums({
+            length: 3,
+            total: 20,
+            excludes: [3, 4],
+        })
 
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div>
+                <section className="hero is-dark is-bold has-text-centered">
+                    <div className="hero-body">
+                        <div className="container">
+
+                            <h1 className="title is-uppercase">Kakuro Helper</h1>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="section columns">
+                    <div className="column">
+                        <div className="has-text-centered is-size-4 has-text-weight-bold">
+                            Length
+                        </div>
+                        <Input type="number" min={1} max={9} />
+                    </div>
+                    <div className="column">
+                        <div className="has-text-centered is-size-4 has-text-weight-bold">
+                            Total
+                        </div>
+                        <Input type="number" min={1} max={45} />
+                    </div>
+                    <div className="column">
+                        <div className="has-text-centered is-size-4 has-text-weight-bold">
+                            Includes
+                        </div>
+                        <Input />
+                    </div>
+                    <div className="column">
+                        <div className="has-text-centered is-size-4 has-text-weight-bold">
+                            Excludes
+                        </div>
+                        <Input />
+                    </div>
+                </section>
+
+                <section className="section columns">
+                    <div className="column" />
+                    <div className="column">
+                        {result.map((a, i) =>
+                            <p key={i} className="is-size-3">
+                                {a.join("")}
+                            </p>
+                        )}
+                    </div>
+                    <div className="column" />
+                </section>
             </div>
         )
     }
